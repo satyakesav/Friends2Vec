618a421bb3efb399291a6979b36a77b647c8e72c
manvitha-k <manvitha.k18@gmail.com>
2019-04-12 16:15:02 -0500

    train-test-split

---
+++ data/dataSplit.py
@@ -0,0 +1,32 @@
+import os
+import random
+
+cwd = os.getcwd()
+ratingsFile = cwd + '/ratings.txt'
+userDict = {}
+with open(ratingsFile, 'r') as infile:
+    for line in infile:
+        #print(line)
+        if line.strip():
+            userId, restroId, rating = line.split('\t')
+            #print(userId, restroId, rating)
+            if userDict.get(userId) == None:
+                userDict[userId] = [(restroId, rating)]
+            else:
+                userDict[userId].append((restroId,rating))
+
+
+print(len(userDict))
+trainFile = cwd +'/train.txt'
+testFile = cwd +'/test.txt'
+with open(trainFile, 'w') as train, open(testFile, 'w') as test:
+    for user in userDict.keys():
+        userList = userDict[user]
+        random.shuffle(userList)
+        for i in range(len(userList)):
+            data = user + '\t' + userList[i][0] + '\t' + userList[i][1] + '\n'
+            print(data)
+            if i == 0:
+                test.write(data)
+            else:
+                train.write(data)
