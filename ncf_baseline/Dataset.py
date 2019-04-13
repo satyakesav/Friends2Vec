3ec10a8aa2e9f47157251637e5fc192713cae756
rupimanoj <manojreddy029@gmail.com>
2019-04-12 23:16:24 -0500

    regression changes 3

--- ncf_baseline/Dataset.py
+++ ncf_baseline/Dataset.py
@@ -66,7 +66,7 @@ class Dataset(object):
             while line != None and line != "":
                 arr = line.split("\t")
                 user, item, rating = int(arr[0]), int(arr[1]), float(arr[2])
-                if (rating > 0):
-                    mat[user, item] = 1.0
+                #if (rating > 0):
+                mat[user, item] = rating
                 line = f.readline()    
         return mat
